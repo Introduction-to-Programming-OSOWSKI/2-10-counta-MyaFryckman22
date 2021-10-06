37d3264ae072b4e001a4a5e41cc75d3dcd926a74
MyaFryckman22 <817258@mystma.org>
2021-10-06 14:50:08 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function 
+def countA(w):
+    for i in range(0,len(w)):
+        if i == "a":
+            return w[i]
+countA("cat")
+#You can put if statements inside of for loops.  
+#w[0] returns the first letter of a word.
+
+#1 function X 
+#1 for loop  
+#1 if statement
